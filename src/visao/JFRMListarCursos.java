02a0889496938e70afa3348373c0b5f8e8b7e7df
Fael do jooj <joojarromba@gmail.com>
2017-04-28 10:44:06 -0300

    Sem o erro :3

--- src/visao/JFRMListarCursos.java
+++ src/visao/JFRMListarCursos.java
@@ -5,18 +5,32 @@
  */
 package visao;
 
+import controle.CursoControle;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import modelo.CursoBEAN;
+
 /**
  *
  * @author user
  */
 public class JFRMListarCursos extends javax.swing.JFrame {
+    
+    private DefaultTableModel dTable;
+    ArrayList<CursoBEAN> curAL = new ArrayList<CursoBEAN>();
+    CursoControle curC = new CursoControle();
 
     /**
      * Creates new form JFRMListarAlunos
+     * aaaaaaaaaaaaaaaaaaaa
      */
     public JFRMListarCursos() {
         initComponents();
         this.setExtendedState(MAXIMIZED_BOTH);
+        
+        this.preencheTabela();
+        
     }
 
     /**
@@ -30,7 +44,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
         lblName16 = new javax.swing.JLabel();
         lblName17 = new javax.swing.JLabel();
         lblName18 = new javax.swing.JLabel();
@@ -38,7 +51,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
         lblName20 = new javax.swing.JLabel();
         lblName21 = new javax.swing.JLabel();
         lblName22 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         lblName24 = new javax.swing.JLabel();
         jButton4 = new javax.swing.JButton();
@@ -56,18 +68,18 @@ public class JFRMListarCursos extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tabelaContas = new javax.swing.JTable();
+        tabelaCursos = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jSeparator1 = new javax.swing.JSeparator();
-        jTextField2 = new javax.swing.JTextField();
+        TFMostraArea = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        TFMostraNome = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        TAMostraDescricao = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
@@ -84,8 +96,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(204, 255, 204));
 
         jPanel4.setBackground(new java.awt.Color(0, 255, 102));
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/Visao/stagiumPqn.png"))); // NOI18N
 
         lblName16.setFont(new java.awt.Font("MV Boli", 1, 48)); // NOI18N
         lblName16.setText("S");
@@ -108,8 +118,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
         lblName22.setFont(new java.awt.Font("MV Boli", 1, 48)); // NOI18N
         lblName22.setText("M");
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/Visao/1.png"))); // NOI18N
-
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
@@ -124,20 +132,16 @@ public class JFRMListarCursos extends javax.swing.JFrame {
                             .addComponent(lblName19, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblName18, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblName17, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lblName16, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2))
+                            .addComponent(lblName16, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(57, 57, 57))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
                         .addComponent(lblName20, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(76, 76, 76))
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)))
+                        .addGap(76, 76, 76))))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(22, 22, 22)
                 .addComponent(lblName16)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblName17)
@@ -151,8 +155,7 @@ public class JFRMListarCursos extends javax.swing.JFrame {
                 .addComponent(lblName21)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblName22)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
-                .addComponent(jLabel3))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         lblName24.setFont(new java.awt.Font("MV Boli", 1, 36)); // NOI18N
@@ -276,8 +279,8 @@ public class JFRMListarCursos extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        tabelaContas.setBorder(javax.swing.BorderFactory.createMatteBorder(10, 10, 10, 10, new java.awt.Color(0, 255, 102)));
-        tabelaContas.setModel(new javax.swing.table.DefaultTableModel(
+        tabelaCursos.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        tabelaCursos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {}
             },
@@ -285,12 +288,12 @@ public class JFRMListarCursos extends javax.swing.JFrame {
 
             }
         ));
-        tabelaContas.addMouseListener(new java.awt.event.MouseAdapter() {
+        tabelaCursos.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tabelaContasMouseClicked(evt);
+                tabelaCursosMouseClicked(evt);
             }
         });
-        jScrollPane1.setViewportView(tabelaContas);
+        jScrollPane1.setViewportView(tabelaCursos);
 
         jPanel3.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
@@ -302,20 +305,23 @@ public class JFRMListarCursos extends javax.swing.JFrame {
 
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
 
-        jTextField2.setEditable(false);
+        TFMostraArea.setEditable(false);
+        TFMostraArea.setBackground(new java.awt.Color(153, 153, 153));
 
         jLabel4.setText("Área:");
 
         jLabel6.setText("Descrição:");
 
-        jTextField3.setEditable(false);
+        TFMostraNome.setEditable(false);
+        TFMostraNome.setBackground(new java.awt.Color(153, 153, 153));
 
         jLabel5.setText("Nome:");
 
-        jTextArea2.setEditable(false);
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane3.setViewportView(jTextArea2);
+        TAMostraDescricao.setEditable(false);
+        TAMostraDescricao.setBackground(new java.awt.Color(153, 153, 153));
+        TAMostraDescricao.setColumns(20);
+        TAMostraDescricao.setRows(5);
+        jScrollPane3.setViewportView(TAMostraDescricao);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -328,12 +334,12 @@ public class JFRMListarCursos extends javax.swing.JFrame {
                     .addComponent(jLabel5))
                 .addGap(33, 33, 33)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 377, Short.MAX_VALUE))
+                    .addComponent(TFMostraNome)
+                    .addComponent(TFMostraArea, javax.swing.GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE))
                 .addGap(26, 26, 26)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane3)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -358,12 +364,12 @@ public class JFRMListarCursos extends javax.swing.JFrame {
                                 .addGap(0, 8, Short.MAX_VALUE)
                                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(jLabel5)
-                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(TFMostraNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jLabel6))
                                 .addGap(26, 26, 26)
                                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(jLabel4)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(TFMostraArea, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
                 .addContainerGap())
         );
@@ -452,9 +458,21 @@ public class JFRMListarCursos extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void tabelaContasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelaContasMouseClicked
-
-    }//GEN-LAST:event_tabelaContasMouseClicked
+    private void tabelaCursosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelaCursosMouseClicked
+         if( evt.getClickCount()== 1){
+             
+            int x = tabelaCursos.getSelectedRow();
+             TFMostraArea.setText(curAL.get(x).getArea());
+             TFMostraNome.setText(curAL.get(x).getNome());
+             TAMostraDescricao.setText(curAL.get(x).getDescricao());
+             
+             
+        }else{
+            
+             JFRMCadastrarCursos cc = new JFRMCadastrarCursos();
+             cc.setVisible(true);
+        } 
+    }//GEN-LAST:event_tabelaCursosMouseClicked
 
     /**
      * @param args the command line arguments
@@ -495,6 +513,9 @@ public class JFRMListarCursos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea TAMostraDescricao;
+    private javax.swing.JTextField TFMostraArea;
+    private javax.swing.JTextField TFMostraNome;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -504,8 +525,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
     private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
@@ -530,9 +549,6 @@ public class JFRMListarCursos extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator5;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField9;
     private javax.swing.JLabel lblName16;
@@ -543,6 +559,56 @@ public class JFRMListarCursos extends javax.swing.JFrame {
     private javax.swing.JLabel lblName21;
     private javax.swing.JLabel lblName22;
     private javax.swing.JLabel lblName24;
-    private javax.swing.JTable tabelaContas;
+    private javax.swing.JTable tabelaCursos;
     // End of variables declaration//GEN-END:variables
+
+    private void preencheTabela() {
+        dTable = criaTabela();
+        //seta o nome das colunas da tabela
+        dTable.addColumn("Código");
+        dTable.addColumn("Nome");
+        dTable.addColumn("Área");
+        dTable.addColumn("Descrição");
+        this.povoaTabela();
+    }
+
+    private DefaultTableModel criaTabela() {
+        //sempre que usar JTable é necessário ter um DefaulttableModel
+        DefaultTableModel dTable = new DefaultTableModel() {
+            //Define o tipo dos campos (coluna) na mesma ordem que as colunas foram criadas
+            Class[] types = new Class[]{
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            //define se os campos podem ser editados na propria tabela
+            boolean[] canEdit = new boolean[]{
+                false, false, false, false};
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit[columnIndex];
+            }
+        ;
+
+        };
+        //retorna o DefaultTableModel
+    return dTable;
+    }
+
+    private void povoaTabela() {
+         this.limparTabela();
+        curAL = curC.povoaTabela();
+        //cada célula do arrayList vira uma linha(row) na tabela
+        for (CursoBEAN dado : curAL) {
+            dTable.addRow(new Object[]{dado.getCodigo(), dado.getNome(),dado.getArea(), dado.getDescricao()});
+        }
+        //set o modelo da tabela
+        tabelaCursos.setModel(dTable);
+    }
+
+    private void limparTabela() {
+        while (tabelaCursos.getRowCount() > 0) {
+    DefaultTableModel dm = (DefaultTableModel) tabelaCursos.getModel();
+    dm.getDataVector().removeAllElements();
+    }
+    }
 }
